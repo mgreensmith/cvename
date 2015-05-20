d049abb5b5bd9ae19092bc2c44972a8ef4bc842a
Matt Greensmith <matt.greensmith@gmail.com>
2015-05-20 00:30:21 -0700

    add Google Analytics

--- js/main.js
+++ js/main.js
@@ -12,8 +12,29 @@ function getRandomSubarray(arr, size) {
     return shuffled.slice(min);
 }
 
+/**
+ * Utility to wrap the different behaviors between W3C-compliant browsers
+ * and IE when adding event handlers.
+ *
+ * @param {Object} element Object on which to attach the event listener.
+ * @param {string} type A string representing the event type to listen for
+ *     (e.g. load, click, etc.).
+ * @param {function()} callback The function that receives the notification.
+ */
+function addListener(element, type, callback) {
+ if (element.addEventListener) element.addEventListener(type, callback);
+ else if (element.attachEvent) element.attachEvent('on' + type, callback);
+}
+
 function newWord() {
   document.getElementById("name").innerHTML = getRandomSubarray(words, 2).join('');
 }
 
 newWord();
+
+var newWordLink = document.getElementById('again');
+
+addListener(newWordLink, 'click', function() {
+  newWord();
+  ga('send', 'event', 'button', 'click', 'newWordLink');
+});
